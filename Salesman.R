5c269377d6aee93c140494070a0a4acb1ce902d5
Artem Shramko <art.shramko@gmail.com>
2018-11-17 12:28:51 +0100

    Update Salesman.R

--- Salesman.R
+++ Salesman.R
@@ -1,31 +1,74 @@
-library(ggplot2)
+City <- setClass(
+        #name of the class
+        "City",
+        
+        #defining slots
+        #in this case its coordinates
+        
+        slots = c(
+                  x = "numeric",
+                  y = "numeric"
+                  ),
+        #setting default values for the slots (optional)
+        prototype = list(
+                  x=0,
+                  y=0
+                  )
+        )
 
-city <- c(index,x,y)
+#calculating distance from the city to another city
+setGeneric(name="getDistance",
+           def=function(theObject, city)
+           {
+             standardGeneric("getDistance")
+           }
+           )
+setMethod(f="getDistance",
+          signature = "City",
+          definition = function(theObject, city)
+          {
+            xDis <- abs(theObject@x-city@x)
+            yDis <- abs(theObject@y-city@y)
+            distance <- sqrt(xDis^2+yDis^2)
+            return(distance)
+          }
+          )
 
-city_distance <- function(x1, y1, x2, y2){
-  xDis <- abs(x1-x2)
-  yDis <- abs(y1-y2)
-  distance <- sqrt(xDis^2+yDis^2)
-  return(distance)
-}
 
-route_distance <- function(route){
-  pathDistance <- 0
-  for (i in 1:len(route)){
-    fromCity <- route[i]
-    toCity <- NaN 
-    if(i+1 < len(route)){
-      toCity <- route[i+1]
-    } else {
-      toCity <- route[0]
-    }
-    pathDistance <- pathDistance + 
-      city_distance(fromCity[2],fromCity[3], toCity[2],toCity[3])
-  } 
-  return(pathDistance)
-}
+Fitness <- setClass("Fitness",
+                    slots=c(
+                      route="list",
+                      distance="numeric",
+                      fitness="numeric"
+                    )
+                    )
+#calculating route distance
 
-route_Fitness <- function(distance){
-  fitness <- 1/distance
-  return(distance)
-}
+#REDO AS A FUNCTION!!!!!!!!!!!!!!!!!!!!
+
+setGeneric(name="routeDistance",
+           def=function(theObject)
+           {standardGeneric("routeDistance")}
+)
+setMethod(f="routeDistance",
+          signature = "Fitness",
+          definition = function(theObject)
+          {
+            if(theObject@distance==0)
+            {
+              pathDistance <- 0
+              for(i in 1:length(theObject@route))
+              {
+                fromCity <- theObject@route[i][[1]]
+                toCity <- NaN
+                if(i+1 < length(theObject@route))
+                {
+                  toCity <- theObject@route[i+1][[1]]
+                } else {toCity <- theObject@route[1][[1]]}
+                pathDistance <- pathDistance + getDistance(fromCity, toCity)
+              }
+              theObject@distance <- pathDistance
+            }
+            return(theObject@distance) 
+          }
+)
