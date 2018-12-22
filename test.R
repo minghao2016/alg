2ffb8fbca66da48a51ec8da824414491fd510047
Artem Shramko <art.shramko@gmail.com>
2018-12-22 17:40:40 +0100

    Update test.R

--- test.R
+++ test.R
@@ -1,12 +1,13 @@
-# clearing the memory
 rm(list = ls())
+
+library(dplyr)
 
 cd <- dirname(rstudioapi::getActiveDocumentContext()$path)
 setwd(dirname(cd))
 
 # setting inner folders
-code.folder <- "code"
-data.folder <- "data"
+code.folder <- "R/code"
+data.folder <- "R/data"
 file.folder <- "R/files"
 resu.folder <- "R/output"
 
@@ -14,8 +15,6 @@ resu.folder <- "R/output"
 source(file.path(code.folder, "nsgaiii.R"))
 source(file.path(code.folder, "obj_funs.R"))
 source(file.path(code.folder, "model.R"))
-
-
 
 #getting input ready
 
@@ -29,41 +28,42 @@ df <- df %>%
                        "BAD" = "0", 
                        "GOOD" = "1"))
 df <- df %>% select(-BAD)
- 
 
+
+costs <- runif(n = 20, min = 1, max = 20)
+names(costs) <- colnames(df[1:(ncol(df)-1)])
 #specify the name of the target column in the data
 #target <- "GOOD"
 #n <- 50 # number of individs per population
 
 obj_list <- c(f_auc, emp) #get_spec) #list of objective functions
-obj_names <- c("auc", "emp" , "nf") #names of objective fns will be used as column names
+obj_names <- c("auc", "emp", "nf", "fcost") #, "nf") #names of objective fns will be used as column names
 
 #specify pareto criteria
-pareto <- high(auc)*high(emp)*low(nf) # high = maximize
+pareto <- low(auc)*low(emp)*low(nf)*low(fcost) # high = maximize
 
-s<-timestamp()
+start_time <- Sys.time()
 
 ans <- alg(df, "GOOD", obj_list, obj_names, pareto, 
-           n = 50, max_gen = 50, 
+           n = 5, max_gen = 5, 
            model = xgb_learner,
            resampling = resampling,
            num_features = TRUE,
-           mutation_rate = 0.2)
+           feature_cost = costs,
+           mutation_rate = 0.01)
 
+end_time <- Sys.time()
+end_time - start_time
 ans
-
-e <- time.timestamp()
-e-s
-
 
 #if you want to visualize the result in terms of objectives
 
-pop <- ans
+pop <- ans$fin_pop
 epop <- evaluate_population(pop = pop,df = df, target = "GOOD", model = xgb_learner,
                             objectives = obj_list, num_features = TRUE)
-colnames(epop)<-c("tpr", "tnr", "acc", "nf")
+colnames(epop)<-c("auc", "emp", "nf")
 spop <- non_dom_sort(epop, pareto)
-plt <- plot_ly(spop, x=~tpr, y=~tnr, z=~nf, 
+plt <- plot_ly(spop, x=~auc, y=~emp, z=~nf, 
                color= ~.level, type="scatter3d", mode = 'markers')
 plt
 
@@ -85,6 +85,4 @@ for(i in 1:20){
 }
 
 colnames(report) <- colnames(df)[-length(df)]
-
-
-cols
+report
