ba2346e3cb9cfb4e74e907f334e2a02e9383932d
Artem Shramko <art.shramko@gmail.com>
2018-12-27 14:01:29 +0200

    Update alg.R
    renamed variables for readability

--- alg.R
+++ alg.R
@@ -447,9 +447,9 @@ maj_vote <- function(pop){
   return(votes)
 }
 
-prep_output <- function(pop., epop.){
-  rownames(epop.) <- 1:nrow(epop.)
-  sorted_fin_pop <- non_dom_sort(epop., pareto)
+prep_output <- function(pop., evaluated_pop.){
+  rownames(evaluated_pop.) <- 1:nrow(evaluated_pop.)
+  sorted_fin_pop <- non_dom_sort(evaluated_pop., pareto)
   pf <- sorted_fin_pop[which(sorted_fin_pop$.level==1),]
   pf <- pf[,-ncol(pf)]
   ids <- rownames(pf)
@@ -468,13 +468,13 @@ prep_output <- function(pop., epop.){
 
 
 #iterator for selecting points from current generation
-select_next_generation <- function(sorted_comb_pop, combined_pop, rp, n){
+select_next_generation <- function(sorted_evaluated_comb_pop, combined_pop_individuals, rp, n){
   next_pop = c()
   #n <- 50
   lvl <- 1
   while(length(next_pop) != n){
     
-    pf <- sorted_comb_pop[which(sorted_comb_pop$.level==lvl),]
+    pf <- sorted_evaluated_comb_pop[which(sorted_evaluated_comb_pop$.level==lvl),]
     pf <- pf[,-ncol(pf)]
     
     len <- length(next_pop)
@@ -501,11 +501,11 @@ select_next_generation <- function(sorted_comb_pop, combined_pop, rp, n){
   eval_next_gen = data.frame()
   for(i in 1:length(next_pop)){
     id <- as.integer(next_pop[i])
-    eval_next_gen <- rbind(eval_next_gen, sorted_comb_pop[id,-ncol(sorted_comb_pop)])
+    eval_next_gen <- rbind(eval_next_gen, sorted_evaluated_comb_pop[id,-ncol(sorted_evaluated_comb_pop)])
   }
   ids <- rownames(eval_next_gen)
   ids <- sapply(ids, as.integer)
-  next_gen <- combined_pop[ids]
+  next_gen <- combined_pop_individuals[ids]
   
   ans <- list(next_gen, eval_next_gen)
   return(ans)
@@ -535,59 +535,59 @@ alg <- function(df, target, obj_list, obj_names,
   
   print("- Generating initial population...")
   #generating initial population
-  ipop <- generate_init_pop(df, n)    
+  initial_pop <- generate_init_pop(df, n)    
   
   #getting values for objective functions
-  epop <- evaluate_population(pop = ipop,df = df, target = target, 
+  evaluated_pop <- evaluate_population(pop = initial_pop, df = df, target = target, 
                               objectives = obj_list, 
                               model = model,
                               resampling = resampling,
                               num_features = num_features,
                               feature_cost = feature_cost)
-  colnames(epop)<-obj_names
+  colnames(evaluated_pop)<-obj_names
   
   current_generation <- 0
   
-  pop <- ipop
+  pop <- initial_pop
   
   print("Performing iterations: ")
-  
+   
   while(current_generation < max_gen){
     
     
     # assigning new id's to previously selected pouints
-    rownames(epop) <- 1:nrow(epop)
+    rownames(evaluated_pop) <- 1:nrow(evaluated_pop)
     
     #crossover
     children <- create_children(pop) 
     
     #mutation
-    mchildren <- mutate_pop(children, 0.1)
+    mutated_children <- mutate_pop(children, 0.1)
     
     #evaluate obj fns for children
-    echildren <- evaluate_population(pop = mchildren,df = df, target = target, 
+    evaluated_children <- evaluate_population(pop = mutated_children,df = df, target = target, 
                                      objectives = obj_list, 
                                      model = model,
                                      num_features=num_features,
                                      feature_cost = feature_cost)
-    colnames(echildren) <- obj_names
-    rownames(echildren) <- (length(pop)+1):(length(pop)+length(children))
+    colnames(evaluated_children) <- obj_names
+    rownames(evaluated_children) <- (length(pop)+1):(length(pop)+length(children))
     
     #combine parent and child
-    combined_pop <- c(pop, mchildren) #individuals with actual binary vectors
+    combined_pop_individuals <- c(pop, mutated_children) #individuals with actual binary vectors
     
-    comb_pop <- rbind(epop,echildren) #id's of individuals with obj funs values
+    evaluated_comb_pop <- rbind(evaluated_pop,evaluated_children) #id's of individuals with obj funs values
     
     #non-dominated sort
-    sorted_comb_pop <- non_dom_sort(comb_pop, pareto)
+    sorted_evaluated_comb_pop <- non_dom_sort(evaluated_comb_pop, pareto)
     
     #select individs for next generation
-    res <- select_next_generation(sorted_comb_pop, combined_pop, rp, n)
+    res <- select_next_generation(sorted_evaluated_comb_pop, combined_pop_individuals, rp, n)
     pop <- res[[1]]
-    epop <- res[[2]]
+    evaluated_pop <- res[[2]]
     
     
-    ideal_point <- compute_ideal_point(sorted_comb_pop)[-length(sorted_comb_pop)]
+    ideal_point <- compute_ideal_point(sorted_evaluated_comb_pop)[-length(sorted_evaluated_comb_pop)]
     current_generation <- current_generation + 1
     
     print(paste0("- Iteration ", current_generation, "/", max_gen))
@@ -595,7 +595,7 @@ alg <- function(df, target, obj_list, obj_names,
     
   }
   
-  result <- prep_output(pop, epop)
+  result <- prep_output(pop, evaluated_pop)
   
   end_time <- Sys.time()
   print(paste("Time: ",end_time - start_time))
